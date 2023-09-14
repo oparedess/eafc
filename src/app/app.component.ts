6b5c99a34d12dd49e1c5b3719b0b4151b10ec9b0
oparedess <oparedess@minsait.com>
2023-09-14 01:12:53 -0600

    Se agrega estructura para jugadores.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EAFC';
+  private URL = '../assets/json/players.json';
+  data: any;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.httpClient.get(this.URL).subscribe(data => this.data = data);
+  }
 }
